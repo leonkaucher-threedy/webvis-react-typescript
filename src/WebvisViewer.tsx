b53949491914a456e8eb08bdc7b0eae947bd6d6c
Jeremy Ondrey <jeremy.ondrey@threedy.io>
2023-03-23 15:15:34 +0100

    Add own default context

--- src/WebvisViewer.tsx
+++ src/WebvisViewer.tsx
@@ -5,11 +5,11 @@ type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any }>;
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      ['webvis-viewer']: CustomElement<HTMLDivElement>;
+      ['webvis-viewer']: CustomElement<HTMLDivElement> & { context?: string };
     }
   }
 }
 
 export default function WebvisViewer(): JSX.Element {
-  return <webvis-viewer></webvis-viewer>;
+  return <webvis-viewer context='myContext'></webvis-viewer>;
 }
