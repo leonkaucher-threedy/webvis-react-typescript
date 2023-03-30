df331b4992a7f7a287625ccb18cd27a955c9c6fd
sascha-raesch <sascha.raesch@threedy.io>
2023-03-30 21:25:21 +0200

    added/changed: comments to webvis component and cleaned it up

--- src/Webvis.tsx
+++ src/Webvis.tsx
@@ -1,38 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import useExternalScripts from './hooks/useExternalScripts';
 import WebvisViewer from './WebvisViewer';
 
 export interface WebvisProps {
-  webvisJS: string;
-  onWebvisReady?: (ctx: webvis.ContextAPI) => void;
-  onWebvisError?: (errorMessage: string) => void;
+    /**
+     * The URL to the webvis JS library that is provided by your instant3Dhub installation.
+     * E.g.: https://hubdemo.threedy.io/repo/webvis/webvis.js
+     */
+    webvisJS: string;
+
+    /**
+     * Optional callback that is triggered as soon as webvis has been loaded.
+     * 
+     * @param ctx - The context to the initialized webVis.
+     */
+    onWebvisReady?: (ctx: webvis.ContextAPI) => void;
+
+    /**
+     * Optional callback if initializing webvis failed.
+     * 
+     * @param errorMessage - Message that describes why initializing webvis failed.
+     */
+    onWebvisError?: (errorMessage: string) => void;
 }
 
+/**
+ * The central webvis react component that contains the webvis viewer component.
+ */
 export default function Webvis(props: WebvisProps): JSX.Element {
+  const {onWebvisReady} = props;
   const [scriptLoaded, setScriptLoaded] = useState<boolean>(false);
-  const [contextReady, setContextReady] = useState<boolean>(false);
+  const [context, setContext] = useState<webvis.ContextAPI | undefined>(undefined);
 
-  useExternalScripts(
-    props.webvisJS,
-    () => setScriptLoaded(true),
-    props.onWebvisError
-  );
+  useExternalScripts(props.webvisJS, () => setScriptLoaded(true), props.onWebvisError);
 
+  // hook to trigger webvis ready callback
   useEffect(() => {
     (async () => {
-      if (scriptLoaded) {
-        const ctx =
-          webvis.getContext('myContext') ??
-          (await webvis.requestContext('myContext'));
-        setContextReady(true);
-        if (props.onWebvisReady && ctx !== undefined) {
-          props.onWebvisReady(ctx);
+      if (scriptLoaded && !context) {
+        const ctx: webvis.ContextAPI | undefined = webvis.getContext('myContext') ?? (await webvis.requestContext('myContext'));
+        setContext(ctx);
+        if (onWebvisReady && ctx !== undefined) {
+          onWebvisReady(ctx);
         }
       }
     })();
-  }, [scriptLoaded, props.onWebvisReady]);
+  }, [scriptLoaded, onWebvisReady, setContext]);
 
-  if (scriptLoaded && contextReady) {
+  if (scriptLoaded && context) {
     return <WebvisViewer />;
   } else {
     return <div></div>;
