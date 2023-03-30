2135d84154246ccb45b998c29e864694917d2705
sascha-raesch <sascha.raesch@threedy.io>
2023-03-30 09:58:38 +0200

    changed: reworked use external scripts hook

--- src/hooks/useExternalScripts.ts
+++ src/hooks/useExternalScripts.ts
@@ -1,49 +1,39 @@
 import { useEffect, useRef } from 'react';
 
+/**
+ * Hook to load an external script.
+ * 
+ * @param url - The URL of the JS script to load.
+ * @param onLoad - Optional callback if the script has been successfully loaded
+ * @param onError - Optional callback if loading the script failed with provided error message.
+ */
 export default function useExternalScripts(url: string, onLoad?: () => void,
     onError?: (errorMessage: string) => void) {
-    const triggeredLoading = useRef<boolean>(false);
+
+    // stores the created script tag
     const scriptRef = useRef<HTMLScriptElement | null>(null);
 
+    // effect to create the script tag and store it in the respective script ref variable
     useEffect(() => {
-        const head = document.querySelector("head") as HTMLElement;
-
         if (scriptRef.current === null) {
-            scriptRef.current = document.createElement("script");
-            head.appendChild(scriptRef.current);
-        }
-    }, []);
+            const scriptTag = document.createElement("script");
+            scriptRef.current = scriptTag;
 
-    useEffect(() => {
-        if (scriptRef.current) {
-            scriptRef.current.setAttribute("src", url);
-        }
-    }, [url, scriptRef]);
-
-    useEffect(() => {
-        if (scriptRef.current && !triggeredLoading.current) {
-            triggeredLoading.current = true;
-
-            const handleScriptLoaded = () => {
+            scriptTag.setAttribute("src", url);
+            scriptTag.onload = () => {
                 if (onLoad) {
                     onLoad();
                 }
             };
-
-            const handleScriptError = (event: Event | string, source?: string, lineno?: number, colno?: number, error?: Error) => {
+            scriptTag.onerror = (event: Event | string, source?: string, lineno?: number, colno?: number, error?: Error) => {
                 if (onError) {
-                    let errorMessage = 'Webvis konnte nicht geladen werden!!!';
-
-                    if(error) {
-                        errorMessage = error.message;
-                    }
-
+                    const errorMessage = error ? error.message : `Could not load script ${url}`;
                     onError(errorMessage);
                 }
-            };
+            }
 
-            scriptRef.current.onload = handleScriptLoaded;
-            scriptRef.current.onerror = handleScriptError;
+            const head = document.querySelector("head") as HTMLElement;
+            head.appendChild(scriptRef.current);
         }
-    }, [onLoad, scriptRef, triggeredLoading, onError]);
+    }, [onLoad, onError, url]);
 };
