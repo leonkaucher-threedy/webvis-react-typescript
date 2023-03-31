84199088d145904fcdb7d530ddcb695c48f52b22
sascha-raesch <sascha.raesch@threedy.io>
2023-03-31 08:14:38 +0200

    changed: updated about text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,11 +6,11 @@ function About(): JSX.Element {
         <h1>About this app</h1>
         <div style={{display: "flex", justifyContent: "center"}}>
           <p className="about">
-            This app demonstrates how to integrate webvis into the React framework using typescript,
+            This app demonstrates how to integrate webvis into the React framework using TypeScript,
             functional components and hooks. To keep an instance of webvis alive and accessible throughout
-            the entire application, it is created in <code>App.tsx</code> and
-            stays in the DOM, and is hidden if switched to other pages. Removing webvis from the DOM also
-            releases all allocated resources on the GPU as the get canvas gets destroyed.
+            the entire application, it stays in the DOM, and is hidden via CSS if switched to other pages.
+            Removing webvis from the DOM would release all allocated resources on the GPU as the get canvas gets destroyed.
+            
           </p>
         </div>
       </div>
